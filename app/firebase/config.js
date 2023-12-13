c5dba8ce63f3b92b0eb4975ff9c0879f8573b92b
talhaunas1 <mtalha4600@gmail.com>
2023-12-13 12:14:47 +0500

    fix issues

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -2,19 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MESSUREMENT_ID,
+export const firebaseConfig = {
+  apiKey: "process.env.NEXT_PUBLIC_FIREBASE_API_KEY",
+  authDomain: "process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN",
+  projectId: "process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID",
+  storageBucket: "process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "process.env.NEXT_PUBLIC_FIREBASE_APP_ID",
+  measurementId: "process.env.NEXT_PUBLIC_FIREBASE_MESSUREMENT_ID",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); 
 export const auth = getAuth(app)
 
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-// export {app, auth}
+
