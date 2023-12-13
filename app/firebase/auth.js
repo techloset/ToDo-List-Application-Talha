3084ee65895bcaf47f2c466aca53afa6498b7498
talhaunas1 <mtalha4600@gmail.com>
2023-12-13 14:16:33 +0500

    displaying errors

--- app/firebase/auth.js
+++ app/firebase/auth.js
@@ -24,6 +24,7 @@ export default function useFirebaseAuth() {
       email: user.email,
       username: user.displayName,
     });
+    
   };
 
   const signOut = () => {
--- app/page.js
+++ app/page.js
@@ -53,7 +53,6 @@ const Page = () => {
 
       fetchTodos(authUser.uid);
       setTodoInput("");
-      
     } catch (e) {
       console.error("Error adding/updating document: ", e);
     }
@@ -89,63 +88,69 @@ const Page = () => {
   };
 
   return (
-    <div className="container mx-auto p-6 bg-white shadow-lg rounded-lg">
-      <h1 className="text-3xl font-semibold mb-6 text-gray-800">Todo Form</h1>
-      <div className="flex flex-wrap items-center">
+    <div className="container mx-auto p-6 bg-gray-700 shadow-lg rounded-lg">
+      <h1 className="text-3xl font-semibold mb-6 text-white text-center">
+        Todo Application Form
+      </h1>
+      <div className="flex flex-wrap items-center justify-center">
         <input
           type="text"
           onChange={(e) => setTodoInput(e.target.value)}
           value={todoInput}
           placeholder="Enter your Todo Here"
-          className="w-full md:w-3/4 bg-gray-100 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-4 md:mb-0"
+          className="w-full md:w-3/4 bg-gray-300 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-4 md:mb-0"
           required
         />
         <button
           onClick={handleSubmit}
-          className="w-full md:w-1/4 bg-blue-500 text-white py-2 px-4 rounded-md text-sm md:ml-2.5  md:mt-6 mt-4 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-600"
+          className="w-full md:w-1/4 bg-blue-700 text-white py-2 px-4 rounded-md text-sm md:ml-2.5 md:mt-6 mt-4 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-600"
         >
           {editingTodoId ? "Update" : "Submit"}
         </button>
       </div>
-
-      {todo.map((item, index) => {
-        return (
-          <div
-            key={item.id}
-            className="mt-4 flex items-center  justify-between gap-2"
-          >
-            <div className="flex  items-center justify-between w-[100%]">
-              <input
-                type="text"
-                value={item.content}
-                readOnly
-                className="w-full  bg-gray-200 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-2"
-              />
-              {editingTodoId !== item.id && (
-                <button
-                  onClick={() => onEditHandler(item.id)}
-                  className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-md text-sm hover:bg-blue-600 focus:outline-none focus:ring-red-300"
-                >
-                  Edit
-                </button>
-              )}
-            </div>
-            <div>
-              {editingTodoId !== item.id && (
-                <button
-                  onClick={() => onDeleteHandler(item.id)}
-                  className="bg-red-500 text-white py-2 px-4 rounded-md text-sm hover:bg-red-600 focus:outline-none focus:ring-red-300"
-                >
-                  Delete
-                </button>
-              )}
-            </div>
+      <h1 className="text-3xl font-semibold mb-6 text-white mt-6">
+        Your Todo's{" "}
+      </h1>
+      {/* <p className="text-white text-center mb-4">
+        Logged in as: {authUser ? authUser.displayName : ''}
+      </p> */}
+      {todo.map((item, index) => (
+        <div
+          key={item.id}
+          className="mt-4 flex items-center justify-between gap-2"
+        >
+          <div className="flex items-center justify-between w-[100%]">
+            <input
+              type="text"
+              value={item.content}
+              readOnly
+              className="w-full bg-gray-200 border border-gray-300 text-gray-800 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-2"
+            />
+            {editingTodoId !== item.id && (
+              <button
+                onClick={() => onEditHandler(item.id)}
+                className="ml-2 bg-blue-700 text-white py-2 px-4 rounded-md text-sm hover:bg-blue-600 focus:outline-none focus:ring-red-300"
+              >
+                Edit
+              </button>
+            )}
           </div>
-        );
-      })}
+          <div>
+            {editingTodoId !== item.id && (
+              <button
+                onClick={() => onDeleteHandler(item.id)}
+                className="bg-red-500 text-white py-2 px-4 rounded-md text-sm hover:bg-red-600 focus:outline-none focus:ring-red-300"
+              >
+                Delete
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
+
       <button
         onClick={signOut}
-        className="border border-gray-300 py-2 px-4 mt-4 text-gray-800 rounded-md text-sm"
+        className="bg-green-500 text-white py-2 px-4 mt-2 rounded-md text-sm hover:bg-red-600 focus:outline-none focus:ring-red-300"
       >
         Sign Out
       </button>
